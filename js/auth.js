a2f7aba3435f61ebd2b671cc75c733fce7452275
Muhammad Dandy Nugraha <dandy7251@gmail.com>
2026-03-25 22:09:23 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,22 @@
+async function login(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email, password
+  });
+
+  if(error) return alert(error.message);
+
+  const user = data.user;
+
+  const { data: profile } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", user.id)
+    .single();
+
+  if(profile.role === "mahasiswa") window.location = "mahasiswa.html";
+  if(profile.role === "umkm") window.location = "umkm.html";
+  if(profile.role === "umum") window.location = "umum.html";
+}
